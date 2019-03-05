697c3575081434267709fd47208bfb5c1386258e
heather <unphurlng@gmail.com>
2019-03-05 14:59:45 -0800

    add hashchange event listener to window

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import movieData from '../data/data.js';
 import loadGallery from './makeGalleryTemplate.js';
+import { readFromQuery } from './query-components.js';
 import './searchComponents.js';
 
 loadGallery(movieData);
+
+window.addEventListener('hashchange', () => {
+    const existingQuery = window.location.hash.slice(1);
+    const queryObject = readFromQuery(existingQuery);
+    console.log(queryObject);
+});
