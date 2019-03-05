4c03d4f18a68b26c0eb8d56d4173039a415113ad
heather <unphurlng@gmail.com>
2019-03-05 11:15:47 -0800

    dynamic test green movie template

--- test/movie-gallery-template.test.js
+++ test/movie-gallery-template.test.js
@@ -2,17 +2,24 @@ const test = QUnit.test;
 
 QUnit.module('TEMPLATE GALLERY TEST');
 
-function makeGalleryTemplate() {
+const movie = {
+    title: 'Star Wars',
+    release_date: 1977,
+    poster_path: '/btTdmkgIvOi0FFip1sPuZI2oQG6.jpg'
+};
+
+
+function makeGalleryTemplate(movie) {
     return `
         <li>
-            <p class="title">Star Wars</p>
-            <img src="https://image.tmdb.org/t/p/w1280/btTdmkgIvOi0FFip1sPuZI2oQG6.jpg">
-            <p class="release">1977</p>
+            <p class="title">${movie.title}</p>
+            <img src="https://image.tmdb.org/t/p/w1280${movie.poster_path}">
+            <p class="release">${movie.release_date}</p>
         </li>
     `;
 }
 
-test('Cheater Test', assert => {
+test('populate each movie to gallery', assert => {
     // arrange
     const expected = `
         <li>
@@ -22,8 +29,10 @@ test('Cheater Test', assert => {
         </li>
     `;
     // act
-    const result = makeGalleryTemplate();
+    const result = makeGalleryTemplate(movie);
 
     // assert
     assert.equal(result, expected);
 });
+
+
